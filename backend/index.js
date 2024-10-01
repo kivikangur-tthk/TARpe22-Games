1cd9fbca7d6618c461bf12da0c7dd8ae896650c0
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2024-10-01 12:19:01 +0300

    GM-4 #comment Add swagerUI and basic swagger.json

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
-const app = require("express")()
 const port = 8080
+const app = require("express")()
+const swaggerUi = require("swagger-ui-express")
+const swaggerDoc = require("./docs/swagger.json")
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)

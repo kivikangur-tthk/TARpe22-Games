988fa770827c6b7f7725e13efac7c744bda55e13
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2024-10-03 14:01:36 +0300

    Add get / route with a link to /docs

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const port = 8080
+const host = 'localhost'
 const app = require("express")()
 const swaggerUi = require("swagger-ui-express")
 const swaggerDoc = require("./docs/swagger.json")
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+app.get("/", (req, res) => {
+    res.send(`Server running. Docs at <a href="http://${host}:${port}/docs">/docs</a>`)
+})
 
 app.get("/games", (req, res) => {
     res.send(["Witcher 3", "Cybepunk 2077"])

87682c337650b7cd26c2cb440ddc41c0686aff07
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2024-10-24 14:19:38 +0300

    Create CounterButton.vue to test reactivity

---
+++ frontend/src/components/CounterButton.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+}
+</script>
+<template>
+  <button @click="count++">You cliced me {{ count }} times.</button>
+</template>
